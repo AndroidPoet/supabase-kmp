e3aa867f99ade92f2ee19d32d6f73dac54ade0bc
Ranbir Singh <poetdroid2@gmail.com>
2026-06-10 00:19:29 +0530

    Bump version to 0.3.4

--- buildSrc/src/main/kotlin/io/github/androidpoet/supabase/Configuration.kt
+++ buildSrc/src/main/kotlin/io/github/androidpoet/supabase/Configuration.kt
@@ -2,7 +2,7 @@ package io.github.androidpoet.supabase
 
 object Configuration {
     const val GROUP = "io.github.androidpoet"
-    const val VERSION = "0.3.3"
+    const val VERSION = "0.3.4"
     const val COMPILE_SDK = 35
     const val MIN_SDK = 21
 }
